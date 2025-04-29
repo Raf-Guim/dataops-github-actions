c4fddaa114f87989ab375ad665c7af6f4da8eac3
Rafael Guimaraes <80708905+Raf-Guim@users.noreply.github.com>
2025-04-28 21:02:59 -0300

    feat: Project iniatialization. Added needed files. Started flask application

---
+++ app/conecta_postegres.py
@@ -0,0 +1,43 @@
+import psycopg2
+import time
+from flask import Flask
+from flask_restx import Api, Resource
+
+app = Flask(__name__)
+
+api = Api(app, version='1.0', title='Alunos API', description='API para conexão com PostgreSQL', doc='/swagger')
+
+ns = api.namespace('alunos', description='Alunos operations')
+
+def get_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="postgres"
+    )
+    return conn
+
+@ns.route('/')
+class Alunos(Resource):
+    def get(self):
+        time.sleep(5)
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("CREATE TABLE IF NOT EXISTS alunos (id serial PRIMARY KEY, nome VARCHAR(255) NOT NULL)")
+        conn.commit()
+
+        cursor.execute("INSERT INTO alunos (nome) VALUES ('Rafael')")
+        conn.commit()
+
+        cursor.execute("INSERT INTO alunos (nome) VALUES ('Maria')")
+        conn.commit()
+
+        cursor.execute("SELECT * FROM alunos")
+        alunos = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return {"alunos": [{"id": aluno[0], "nome": aluno[1]} for aluno in alunos]}
